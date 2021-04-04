3be1fbd196df993c205c1e8aaa91d5f03ec58f0a
Mike Sturm <jmichaelsturm@gmail.com>
2021-04-04 16:05:45 -0500

    Update gems

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,36 +2,36 @@ var store = [{
         "title": "Parable of the Comet",
         "excerpt":"I have a confession - I am an astronomy geek. That may not be much of a surprise to those of you who have seen my 8 inch telescope or watched my excitement looking to the skies from a dark, country location away from the bright lights of the city....","categories": ["Blog"],
         "tags": ["Astronomy","Comet","God","Christianity"],
-        "url": "http://localhost:4000/blog/parable-of-the-comet/",
+        "url": "/blog/parable-of-the-comet/",
         "teaser": null
       },{
         "title": "Christmas Presence",
         "excerpt":"One of my fondest childhood memories of Christmas has to be The Cheetah. No, I am not talking about the fastest of all land mammals. For those of you who were not a young boy in the early 1970’s, the cheetah was the pinnacle of ride-on toys. This was no...","categories": ["Blog"],
         "tags": ["Christmas","God","Christianity"],
-        "url": "http://localhost:4000/blog/christmas-presense/",
+        "url": "/blog/christmas-presense/",
         "teaser": null
       },{
         "title": "A Tribute to My Dad",
         "excerpt":"My Dad, Gene Paul Sturm, Jr., of Bartlesville, OK passed away on March 2, 2020 after years of struggle against Alzheimer’s Disease. Below is a tribute to my father, friend, and mentor. I will never forget him and his tremendous, positive impact on my life. In the book of Matthew,...","categories": ["Blog"],
         "tags": ["Family","Dad","God","Christianity"],
-        "url": "http://localhost:4000/blog/tribute-to-dad/",
+        "url": "/blog/tribute-to-dad/",
         "teaser": null
       },{
         "title": "Nobody eclipses my Mom!",
         "excerpt":"Happy Mother’s Day! I apologize for the cheesy pun in this blog post title, but bear with me as I recall a very special trip spent with my wonderful Mom and two of my children. And… of course this statement is true! As a certified astronomy geek and slightly very...","categories": ["Blog"],
         "tags": ["Astronomy","Eclipse","Family","Mom"],
-        "url": "http://localhost:4000/blog/mothers-day/",
+        "url": "/blog/mothers-day/",
         "teaser": null
       },{
         "title": "Raspberry Pi Aquarium Monitor",
         "excerpt":"Before we get started, I can anticipate many of your questions about this blog post already. “Why do you have a typo in your blog title?” “What does a delicious fruit dessert have to do with aquariums?” If you don’t have a computer geek in your life, you may not...","categories": ["Blog"],
         "tags": ["Raspberry Pi","Linux","Family"],
-        "url": "http://localhost:4000/blog/pi-aquarium-monitor/",
+        "url": "/blog/pi-aquarium-monitor/",
         "teaser": null
       },{
         "title": "Remembering summers with Dad",
         "excerpt":"For my brother Mark and me, this is our first Father’s Day without Dad, who passed away in March. For many years, Alzheimer’s Disease slowly robbed our family of Dad’s full presence. At first, it slightly diminished his exceptional intellect, giving the rest of us a fighting chance to win...","categories": ["Blog"],
         "tags": ["Astronomy","Golf","Gardening","Family","Sports","Summer","Dad"],
-        "url": "http://localhost:4000/blog/fathers-day/",
+        "url": "/blog/fathers-day/",
         "teaser": null
       }]
